97d7c6abc31253bae032755784da7fe5751bf5c9
davidsimpson <mapdes@gmail.com>
2015-06-23 16:44:32 +0100

    Solved problem28

---
+++ problem28.py
@@ -0,0 +1,27 @@
+"""Number spiral diagonals
+Problem 28
+Starting with the number 1 and moving to the right in a clockwise direction a 5
+by 5 spiral is formed as follows:
+
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed
+in the same way?"""
+
+n = 1001 ** 2
+total = 1001 ** 2
+side = 1000
+while n != 1:
+    for d in range(3):
+        n -= side
+        total += n
+    n -= side
+    total += n
+    side -= 2
+print total
